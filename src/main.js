c64361cd5573849e7ce92af75eb1194914148244
handsome <3393402356@qq.com>
2021-04-19 15:04:35 +0800

    创建项目

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入reset.css公共样式文件
+import './assets/css/reset.css'
+
+// 引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
+/* import { request } from './network/request'
+
+request({
+    url: '/product/pagination',
+    params: { page: 1, size: 10 }
+}).then(data => {
+    console.log(data.data);
+}) */
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/network/request.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+export const request = config => {
+    const inst = axios.create({
+            baseURL: '/api/v4',
+            timeout: 10000
+        })
+        // 请求拦截
+    inst.interceptors.request.use(config => {
+            console.log(config)
+            return config
+        })
+        //响应拦截
+    inst.interceptors.response.use(data => {
+        return data.data
+    })
+    return inst(config)
+}
